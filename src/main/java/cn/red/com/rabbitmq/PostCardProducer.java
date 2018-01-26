4a49b7d8892dca918c8603ec617ad5562a1504eb
red <whatwhywhen123@163.com>
2018-01-26 18:08:15 +0800

    通过注解的方式将消费者添加到队列的监听

--- src/main/java/cn/red/com/rabbitmq/PostCardProducer.java
+++ src/main/java/cn/red/com/rabbitmq/PostCardProducer.java
@@ -1,14 +1,18 @@
 package cn.red.com.rabbitmq;
 
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@RabbitListener(queues = "activeQueue")
 public class PostCardProducer {
 	@Autowired
 	private RabbitTemplate amqpTemplate;
 	
+	@RabbitHandler
 	public void produce(){
 		amqpTemplate.convertAndSend("Hello, world!");
 	}
